8cabe6ccf5c8a48fe433c4158aef95c63e3bbe35
typhonrt <support@typhonjs.io>
2023-05-01 20:21:07 -0700

    Update global types.

--- src/globals-fvtt.d.ts
+++ src/globals-fvtt.d.ts
@@ -1,7 +1,12 @@
 declare global {
    export type ApplicationHeaderButton = object;
    export type DocumentCollection = object;
+   export type EditorView = object;
+   export type FontFamilyDefinition = object;
    export type game = object;
+   export type ProseMirrorCommand = object;
+   export type Schema = object;
+
    export namespace foundry {
       namespace abstract {
          export type Document = object;
