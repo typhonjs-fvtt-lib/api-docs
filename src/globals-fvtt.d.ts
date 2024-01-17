d85926cbe531370087a2fd0ed1d2636bfccd224a
typhonrt <support@typhonjs.io>
2024-01-17 13:50:56 -0800

    Add additional Foundry globals.

--- src/globals-fvtt.d.ts
+++ src/globals-fvtt.d.ts
@@ -3,13 +3,13 @@ declare global {
    export type CompendiumCollection = object;
    export type DocumentCollection = object;
    export type EditorView = object;
+   export type FavoriteFolder = object;
    export type Folder = object;
    export type FontFamilyDefinition = object;
    export type game = object;
    export type ProseMirrorCommand = object;
    export type RollTable = object;
    export type Schema = object;
-
    export namespace foundry {
       namespace abstract {
          export type Document = object;
