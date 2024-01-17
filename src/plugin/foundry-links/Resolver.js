c408f1fc11b8e8b8308f7317bc100bb0d0580eb4
typhonrt <support@typhonjs.io>
2024-01-17 13:50:30 -0800

    Update link checker

--- src/plugin/foundry-links/Resolver.js
+++ src/plugin/foundry-links/Resolver.js
@@ -1,6 +1,10 @@
-import { Application }  from 'typedoc';
+import {
+   Application,
+   ProjectReflection,
+   ReflectionKind,
+   RendererEvent }   from 'typedoc';
 
-import { resolvers }    from './resolvers/index.js';
+import { resolvers } from './resolvers/index.js';
 
 export class Resolver
 {
@@ -11,11 +15,11 @@ export class Resolver
    #app;
 
    /**
-    * Stores the `globalThis` and `foundry` namespaced symbols that failed to resolve.
+    * Stores the symbols that failed to resolve.
     *
-    * @type {Set<string>}
+    * @type {Map<string, string>}
     */
-   #failed = new Set();
+   #failed = new Map();
 
    /**
     * Typedoc version support.
@@ -37,24 +41,54 @@ export class Resolver
       this.#supports.objectReturn = +version[1] > 23 || +version[2] >= 26;
 
       this.#app.converter.addUnknownSymbolResolver(this.#handleUnknownSymbol.bind(this));
+
+      this.#app.renderer.once(RendererEvent.END, () =>
+      {
+         if (this.#failed.size)
+         {
+            this.#app.logger.warn('[link-resolver] Failed to resolve the following reflections / types:');
+
+            const keys = [...this.#failed.keys()].sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()));
+            for (const key of keys) { this.#app.logger.warn(this.#failed.get(key)); }
+         }
+      });
+   }
+
+   /**
+    * @param {import('typedoc').Reflection}  reflection -
+    *
+    * @returns {string} The fully qualified symbol name.
+    */
+   #getSymbolName(reflection)
+   {
+      const parts = [];
+
+      while (reflection)
+      {
+         // Do not include the project reflection.
+         if (reflection instanceof ProjectReflection) { break; }
+
+         parts.unshift(reflection.name);
+         reflection = reflection.parent;
+      }
+
+      return parts.join('.');
    }
 
    /**
     * Attempts to resolve an unknown symbol against global, script scope, and foundry namespaced symbols.
     *
-    * @param {import('typedoc').DeclarationReference} ref - Declaration reference.
+    * @param {import('typedoc').DeclarationReference} ref - Unknown symbol reference.
+    *
+    * @param {import('typedoc').Reflection}  refl - Source reflection.
     *
     * @returns {import('typedoc').ExternalResolveResult | string | void} Resolve result.
     */
-   #handleUnknownSymbol(ref)
+   #handleUnknownSymbol(ref, refl)
    {
-      // if (ref.moduleSource === 'typescript' || (!ref.moduleSource && ref.resolutionStart === '__global'))
+      if (ref.moduleSource)
       {
          const symbolPath = ref.symbolReference?.path ?? Resolver.#emptyArray;
-
-         // Only handle symbols that start with `globalThis`. All TRL source code uses `globalThis` to reference
-         // the Foundry API.
-         // if (symbolPath.length < 2 || symbolPath?.[0]?.path !== '__global') { return; }
 
          const name = symbolPath?.map((path) => path.path).join('.');
 
@@ -64,10 +98,12 @@ export class Resolver
 
          for (const resolver of resolvers) { result = resolver(name); }
 
-         if (!result && !this.#failed.has(name))
+         const fullName = `${ref.moduleSource}/${name}`;
+
+         if (!result && !this.#failed.has(fullName))
          {
-            this.#failed.add(name);
-            this.#app.logger.verbose(`[typedoc-plugin-foundry-links]: Failed to resolve type: ${name}`);
+            this.#failed.set(fullName, `[link-resolver] ${name} from ${ref.moduleSource} in ${
+             this.#getSymbolName(refl)} (${ReflectionKind.singularString(refl.kind)})`);
          }
 
          if (this.#supports.objectReturn && result)
