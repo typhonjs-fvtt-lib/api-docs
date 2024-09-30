aedf9cfe3bef4deef2e61d088dedc6ba33f69a41
typhonrt <support@typhonjs.io>
2024-09-30 16:24:13 -0700

    Remove unnecessary prepends.

--- prepend/#runtime/svelte/application/legacy.js
+++
@@ -1,8 +0,0 @@
-/**
- * Provides legacy support for Handlebars rendering while still gaining aspects of reactivity with a Svelte powered
- * application shell. You may use the resources in this package in a similar manner as the core Foundry
- * {@link Application} / {@link FormApplication} classes. This should only be an interim or stepwise solution as you
- * convert your package over to fully using TRL & Svelte.
- *
- * @module #runtime/svelte/application/legacy
- */
