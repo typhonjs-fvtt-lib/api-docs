03dd2b4a8e8f3f053b05a9315406dbd2f7155951
typhonrt <support@typhonjs.io>
2024-10-28 19:15:24 -0700

    Update name

--- src/generate/typedoc/typedoc.js
+++ src/generate/typedoc/typedoc.js
@@ -21,7 +21,7 @@ import {
 export async function typedoc(logLevel = LogLevel.Verbose)
 {
    const app = await Application.bootstrapWithPlugins({
-      name: 'TyphonJS Runtime Library (FVTT) 0.2.0-next.1',
+      name: 'TyphonJS Runtime Library (FVTT) 0.2.0',
 
       // Provide a link for the title / name.
       // titleLink: '',
