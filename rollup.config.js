fbdd72e91f646dff0cecd29beefc40d8c6ef0e27
typhonrt <support@typhonjs.io>
2024-12-03 19:05:30 -0800

    Update config.

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,12 @@
-import resolve          from '@rollup/plugin-node-resolve';
-
 /**
  * @type {import('rollup').RollupOptions}
  */
 export default {
    input: 'src/plugin/foundry-links/index.js',
    external: ['typedoc'],
-   plugins: [
-      // resolve()
-   ],
    output: {
-      file: 'dist/plugin/foundry-links/index.cjs',
-      format: 'cjs',
+      file: 'dist/plugin/foundry-links/index.js',
+      format: 'es',
       generatedCode: { constBindings: true },
       sourcemap: true
    }
